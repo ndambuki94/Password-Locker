0eb1707ba56bf38ad242c9eb9075e8fac140932b
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 11:50:58 +0300

    test to ensure that the object is initialized

--- test_user.py
+++ test_user.py
@@ -2,3 +2,17 @@ import unittest
 from user import User
 
 
+class TestUser(unittest.TestCase):
+    """Test class that defines test cases for the user class behaviour"""
+
+     def setUp(self):
+        """Set Up Method to run before each test case to check if the class has been instantiated correctly"""
+        self.new_user = User("NewUser", "12345")
+
+    def test_init(self):
+        """Test to ensure that the object is initialized properly"""
+        self.assertEqual(self.new_user.user_name, "NewUser")
+        self.assertEqual(self.new_user.password, "12345")
+
+
+        
