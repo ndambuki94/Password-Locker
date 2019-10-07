e17c12f8cb036cc941e06470077488e6f26b1965
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 11:21:19 +0300

    add user empty array

--- user.py
+++ user.py
@@ -1,3 +1,8 @@
 class User:
     """Create class for users"""
+     def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+    user_list = []
+
     
