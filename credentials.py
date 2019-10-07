68d6cb44bd0a10fde65e0f476156c6840cacec89
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 12:59:08 +0300

    add changes

--- credentials.py
+++ credentials.py
@@ -1,13 +1,13 @@
 class Credentials:
     """Create class for credentials"""
 
-     def __init__(self, account_name, account_password):
+    def __init__(self, account_name, account_password):
         self.account_name = account_name
         self.account_password = account_password
 
     credentials_list = []
 
-     def save_credentials(self):
+    def save_credentials(self):
         """Method that saves credential objects into credentials_list"""
         self.credentials_list.append(self)
 
@@ -15,7 +15,7 @@ class Credentials:
         """Method which deletes a particular credential"""
         Credentials.credentials_list.remove(self)
     
-     @classmethod
+    @classmethod
     def find_by_name(cls, account_name):
         """Method that takes in a name and returns a credential that matches that particular name
         Args:
@@ -28,7 +28,7 @@ class Credentials:
             if credential.account_name == account_name:
                 return credential
 
-     @classmethod
+    @classmethod
     def credential_exists(cls, name):
         """Method to check whether a credential exists
         Args:
@@ -41,7 +41,7 @@ class Credentials:
                 return True
         return False
     
-     @classmethod
+    @classmethod
     def display_credentials(cls):
         """Method which displays all current credentials"""
         return cls.credentials_list
