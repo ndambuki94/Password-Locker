a2ef35a4d8349c716b519a81cf88c59e9bb585d7
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 12:37:39 +0300

    function to create new credentials and to save the created credentials

--- run.py
+++ run.py
@@ -4,6 +4,15 @@ import random
 from user import User
 from credentials import Credentials
 
+def create_new_credential(account_name, account_password):
+    """Function to create a new account and its credentials"""
+    new_credential = Credentials(account_name, account_password)
+    return new_credential
+
+
+def save_new_credential(credentials):
+    """Function to save the newly created account and password"""
+    credentials.save_credentials()
 
 
 
