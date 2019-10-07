ed35a58fc964c6ff08d78e3f852c3a49ca8d2197
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 12:06:53 +0300

    method to check proper instantiation

--- credentials_test.py
+++ credentials_test.py
@@ -5,4 +5,13 @@ class TestCredentials(unittest.TestCase):
     """Test class that defines test cases for the Credentials class behavior
     """
 
-    
+     def setUp(self):
+        """Set up method to run befor before each test case"""
+        self.new_credentials = Credentials("Facebook", "12345")
+
+    def test_credentials_instance(self):
+        """Method that tests whether the new_credentials have been instantiated correctly"""
+        self.assertEqual(self.new_credentials.account_name, "Facebook")
+        self.assertEqual(self.new_credentials.account_password, "12345")
+
+
