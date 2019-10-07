d62897561a97be32eb73c41705e7a2fcff3bf20c
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 13:03:10 +0300

    add minor changes

--- test_user.py
+++ test_user.py
@@ -5,7 +5,7 @@ from user import User
 class TestUser(unittest.TestCase):
     """Test class that defines test cases for the user class behaviour"""
 
-     def setUp(self):
+    def setUp(self):
         """Set Up Method to run before each test case to check if the class has been instantiated correctly"""
         self.new_user = User("NewUser", "12345")
 
@@ -14,8 +14,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_name, "NewUser")
         self.assertEqual(self.new_user.password, "12345")
 
-     def test_save_user(self):
+    def test_save_user(self):
         """Method that tests wether an user credentials have been successfully saved"""
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 class User:
     """Create class for users"""
-     def __init__(self, user_name, password):
+    def __init__(self, user_name, password):
         self.user_name = user_name
         self.password = password
     user_list = []
@@ -8,3 +8,6 @@ class User:
     def save_user(self):
         """Method that saves user objects into user_list"""
         self.user_list.append(self)
+
+if __name__ == '__main__':
+    main()
