be70add49ee35acbebc10b790985e3ea3f2a1857
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 12:02:12 +0300

    import unnitest for the credentials

---
+++ credentials_test.py
@@ -0,0 +1,8 @@
+import unittest
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    """Test class that defines test cases for the Credentials class behavior
+    """
+
+    
