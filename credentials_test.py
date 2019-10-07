92c6eff9b894ef84f4288c853445935d52ed07e3
ndambuki94 <ndambuki94@gmail.com>
2019-10-07 12:11:48 +0300

    method to check saving of credentials

--- credentials_test.py
+++ credentials_test.py
@@ -14,4 +14,10 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.account_name, "Facebook")
         self.assertEqual(self.new_credentials.account_password, "12345")
 
+    def test_save_credentials(self):
+        """Method that tests whether the new credential created has been saved"""
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+           
+
 
